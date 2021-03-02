7ee4e8b6c9f7d3846b1f0ed721c2915b749a0a5b
EverKlett <everton.klettenberg@gmail.com>
2021-03-02 19:47:33 -0300

    Updated do set the private modifier to the set function of cpf

--- src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -1,13 +1,24 @@
 package one.digitalInnovation.digionebank
 
 class Pessoa {
-    var nome: String = "Everton";
-    var cpf: String = "123.123.123-12";
+    var nome: String = "Everton"
+
+    var cpf: String = "123.123.123-12"
+    private set
+    /*
+    get e set são inferidos na variável e o private na variável faz get e set receberem private, para privar somente uma
+    funcionalidade pode ser definido como acima, somente é possível definir uma propriedade de visibilidade diferente da
+    variável para o set
+    */
+    inner class Endereco {
+        var rua: String = "Nome de Rua"
+    }
 }
 
 fun main() {
-    val Everton = Pessoa();
+    val everton = Pessoa()
 
-    println(Everton.nome);
-    println(Everton.cpf);
+    println(everton.nome)
+    println(everton.cpf)
+    println(everton.Endereco().rua)
 }
