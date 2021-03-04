87cd481d7d3e2dc1cf1bb49f0f51db0d281866c3
EverKlett <evertonklettenberg@gmail.com>
2021-03-04 20:05:19 -0300

    Transforming classes Pessoa.kt and Funcionario.kt to abstract

--- src/main/kotlin/one/digitalInnovation/digionebank/Testes/TestePessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/Testes/TestePessoa.kt
@@ -1,10 +1,10 @@
 package one.digitalInnovation.digionebank.testes
 
-import one.digitalInnovation.digionebank.Pessoa
+import one.digitalInnovation.digionebank.Funcionario
+import java.math.BigDecimal
 
 fun main() {
-    val everton = Pessoa(cpf = "123.123.123.12",
-                         nome = "Everton")
+    val everton = Funcionario(cpf = "123.123.123.12", nome = "Everton", salario = BigDecimal.valueOf(5555.5))
 
     println(everton.pessoaInfo())
 }
